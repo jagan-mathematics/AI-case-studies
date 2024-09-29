e278d5f1e55d1ba0168d9611e972aed550e06f12
root <root@crmgpu6-10042.csez.zohocorpin.com>
2024-09-29 11:51:44 +0000

    adding rmsenorm

---
+++ Experimentation/blocks/layers/norms.py
@@ -0,0 +1,16 @@
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.zeros(dim))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float())
+        output = output * (1.0 + self.weight.float())
+        return output.type_as(x)
+
+    def extra_repr(self):
+        return f"{tuple(self.weight.shape)}, eps={self.eps}"
